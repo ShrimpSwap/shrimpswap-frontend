c8ba17a7cbc287f69f834fc44e03f75bed2a187a
pedersonShrimp <pedersonshrimp@gmail.com>
2021-04-21 21:29:57 +1000

    fix: Deposit fee

--- src/components/TokenInput/TokenInput.tsx
+++ src/components/TokenInput/TokenInput.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 import BigNumber from 'bignumber.js/bignumber'
 import { Button } from '@shrimpswap/uikit'
+import { getBalanceNumber } from 'utils/formatBalance'
 import useI18n from '../../hooks/useI18n'
 import Input, { InputProps } from '../Input'
 
@@ -46,8 +47,8 @@ const TokenInput: React.FC<TokenInputProps> = ({
       />
       {depositFeeBP > 0 ? (
         <StyledMaxText>
-          {TranslateString(10001, 'Deposit Fee')}: {new BigNumber(value || 0).times(depositFeeBP / 10000).toString()}{' '}
-          {symbol}
+          {TranslateString(10001, 'Deposit Fee')}:{' '}
+          {getBalanceNumber(new BigNumber(depositFeeBP || 0).div(100)).toFixed(4)}%{symbol}
         </StyledMaxText>
       ) : null}
       {burnFee > 0 ? (
