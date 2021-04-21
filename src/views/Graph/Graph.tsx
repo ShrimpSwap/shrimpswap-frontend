c32638a2445e40b5f44d7920dfaa34a12af7f4e7
pedersonShrimp <pedersonshrimp@gmail.com>
2021-04-22 08:22:13 +1000

    feat: Add SHRIMP address for price graph

--- src/views/Graph/Graph.tsx
+++ src/views/Graph/Graph.tsx
@@ -4,7 +4,10 @@ import FullPage from './components/FullPage'
 
 const Graph: React.FC = () => (
   <FullPage>
-    <IFrame title="graph" url="https://goswappcharts.web.app/?isbsc=true&tokenId=TODO" />
+    <IFrame
+      title="graph"
+      url="https://goswappcharts.web.app/?isbsc=true&tokenId=0x62ee12e4fe74a815302750913c3c796bca23e40e"
+    />
   </FullPage>
 )
 
