8306e3e810000c210421750b7f1cbb8a47510330
TigerShrimp21 <kulina.kol@gmail.com>
2021-04-25 15:49:07 +0300

    feat: Fix SALT pool

--- src/config/constants/pools.ts
+++ src/config/constants/pools.ts
@@ -6,7 +6,7 @@ const pools: PoolConfig[] = [
     tokenName: 'SALT',
     stakingTokenName: QuoteToken.SHRIMP,
     stakingTokenAddress: {
-      97: '0x85582d24455caaf44fc4f914bdca2a6cb73e6681',
+      97: '',
       56: '0x62ee12e4fe74a815302750913c3c796bca23e40e', // SHRIMP
     },
     contractAddress: {
@@ -18,7 +18,7 @@ const pools: PoolConfig[] = [
       56: '0x2849b1aE7E04A3D9Bc288673A92477CF63F28aF4', // SALT
     },
     poolCategory: PoolCategory.CORE,
-    projectLink: 'https://https://www.saltswap.finance/',
+    projectLink: 'https://www.saltswap.finance/',
     harvest: true,
     tokenPerBlock: '0.005787037037037037',
     sortOrder: 1,
