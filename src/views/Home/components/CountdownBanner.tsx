7a29ce8ce66278c820fe0814d764467f8ed2ea67
pedersonShrimp <pedersonshrimp@gmail.com>
2021-06-22 13:56:30 +1000

    feat: Add countdown

--- src/views/Home/components/CountdownBanner.tsx
+++ src/views/Home/components/CountdownBanner.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Flex } from '@shrimpswap/uikit'
+import moment from 'moment'
 import styled from 'styled-components'
 import PageContainer from 'components/layout/Container'
 
@@ -17,7 +17,6 @@ const Container = styled(PageContainer)`
 `
 
 const Countdown = styled.div`
-  text-align: center;
   font-size: 24px;
   line-height: 110%;
   white-space: nowrap;
@@ -27,10 +26,17 @@ const Countdown = styled.div`
 const CountdownBanner = () => (
   <StyledCountdownBanner>
     <Container>
-      <Flex alignItems="center" justifyContent={['center', 'center', null, 'start']}>
-        <Countdown>Farming opens on block <a href="https://bscscan.com/block/countdown/8774580">8774580</a> (around Thu Jul 01 2021 12:44:20 GMT+0300) </Countdown>
-      </Flex>
-      <Countdown><a href="https://shrimpswap.gitbook.io/shrimpswap/v2-whale/migration">Migration guide</a></Countdown>
+      <Countdown>
+        Farming opens on block{' '}
+        <a href="https://bscscan.com/block/countdown/8774580" style={{ color: '#88bbdb' }}>
+          8774580
+        </a>
+        {` (${moment.utc('01 07 2021 12:45:00', 'DD MM YYYY hh:mm:ss').fromNow()})`}. Refer to our {` `}
+        <a href="https://shrimpswap.gitbook.io/shrimpswap/v2-whale/migration" style={{ color: '#88bbdb' }}>
+          migration guide
+        </a>
+        .
+      </Countdown>
     </Container>
   </StyledCountdownBanner>
 )
